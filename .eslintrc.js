f196d1486d159e827b05405b941f34f26c564f23
Patricio Gonzalez <patote.gonzalez@gmail.com>
2021-04-15 06:35:16 -0400

    flter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,19 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {
-    'no-console': 0,
-  },
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+	},
+	// extends: [
+	// 	'airbnb-base',
+	// ],
+	parserOptions: {
+		ecmaVersion: 12,
+	},
+	rules: {
+		'no-console': 0,
+		indent: 'off',
+		semi: [2, 'always'],
+		"no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
+	},
 };
--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const app = express();
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
-  .get((req, res) => {
-    const response = { hello: 'this is my books API' };
-    res.json(response);
-  });
+	.get((req, res) => {
+		const { query } = req;
+		Book.find(query, (err, books) => {
+			if (err) { return res.send(err); }
+			return res.json(books);
+		});
+	});
 
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
-  res.send('welcome to my API2!');
+	res.send('welcome to my API2!');
 });
 
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+	console.log(`Running on port ${port}`);
 });
