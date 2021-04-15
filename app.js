96845ebddac8381b41ff23056b12cf5083808eee
Patricio Gonzalez <patote.gonzalez@gmail.com>
2021-04-15 06:51:57 -0400

    add new book

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const app = express();
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 bookRouter.route('/books')
+	.post((req, res) => {
+		const book = new Book(req.body);
+		book.save();
+		return res.status(201).json(book);
+	})
 	.get((req, res) => {
 		const query = {};
 		if (req.query.genre) {
